bb26b86592fed939e78bc33548238da025adc33c
Daya Sharma <vickyshrm257@gmail.com>
2025-03-15 22:34:48 +0530

    Added loggers logic - test resources/log4j2.xml, Updated TestBase Class, testcase class.

--- src/test/java/testBase/TestBaseClass.java
+++ src/test/java/testBase/TestBaseClass.java
@@ -3,6 +3,8 @@ package testBase;
 import java.time.Duration;
 
 import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
@@ -11,8 +13,16 @@ import org.testng.annotations.BeforeClass;
 public class TestBaseClass {
 	
 	public WebDriver driver;
+	public Logger logger;
+	
 	@BeforeClass
 	public void setup() {
+		
+		//creates the logger for the specified class, 
+		// here this.getClass() helps to get the testcase class (As it extends TestBase) and initiates the logger for that particular test class.
+		//System.setProperty("log4j.configurationFile","C:\\Users\\hp\\git\\repository\\OpencartV121\\src\\test\\resources\\log4j2.xml");
+		logger = LogManager.getLogger(this.getClass());
+		//Now this logger can be used in the test classes as setup will execute before each test (BeforeClass)
 		
 		//Setting up the Chrome driver
 		driver = new ChromeDriver();
--- src/test/java/testCases/TC001_AccountRegistrationTest.java
+++ src/test/java/testCases/TC001_AccountRegistrationTest.java
@@ -2,10 +2,12 @@ package testCases;
 
 import static org.testng.Assert.assertEquals;
 
+import java.io.Console;
 import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -19,12 +21,23 @@ public class TC001_AccountRegistrationTest extends TestBaseClass {
 	@Test
 	public void verify_account_registration() throws InterruptedException {
 		
+		logger.info("***** Starting TC001_AccountRegistrationTest *****");
+		System.out.println("***** Starting TC001_AccountRegistrationTest *****");
+		
+		try {
 		//Go to home page and click on My account -> Register
 		HomePage homePage = new HomePage(driver);
 		homePage.clickMyAccount();
+		logger.info("Clicked on my account link");
+		System.out.println("Clicked on my account link");
 		homePage.clickRegister();
+		logger.info("Clicked on my Register link");
+		System.out.println("Clicked on my Register link");
+
 		
 		//Account registration page - Fill form and submit
+		logger.info("Filing account registration form details");
+		System.out.println("Filing account registration form details");
 		AccountRegistrationPage accountRegisterPage = new AccountRegistrationPage(driver);
 		accountRegisterPage.setFirstName(getRandomString(5));
 		accountRegisterPage.setLastName(getRandomString(5));
@@ -32,15 +45,33 @@ public class TC001_AccountRegistrationTest extends TestBaseClass {
 		accountRegisterPage.setEmail(getRandomAlphanumeric(8)+"@mailinator.com");
 		
 		//Account registration page - check privacy check box and submit.
-		Thread.sleep(5000);
+		logger.info("Selecting the privacy policy checkbox");
+		System.out.println("Selecting the privacy policy checkbox");
+		Thread.sleep(2000);
 		accountRegisterPage.checkPrivacyPolicy();
-		Thread.sleep(5000);
+		
+		logger.info("Clicking on continue button to submit the account registrationb form.");
+		System.out.println("Clicking on continue button to submit the account registration form.");
+		Thread.sleep(2000);
 		accountRegisterPage.clickContinue();
 		
 		//Assertions
-		assertEquals(accountRegisterPage.getConfirmationMessage(), 
-				"Your Account Has Been Created!");
+		logger.info("Validating the confirmation message after Submit");
+		System.out.println("Validating the confirmation message after Submit");
+		String confmsg = accountRegisterPage.getConfirmationMessage();
+		assertEquals(confmsg, "Your Account Has Been Created!");
 		
+		logger.info("***** Finished TC001_AccountRegistrationTest Successfully*****");
+		System.out.println("***** Finished TC001_AccountRegistrationTest Successfully*****");
+		}
+		
+		catch(Exception e) {
+			logger.error("Test case failed - TC001_AccountRegistrationTest");
+			System.out.println("Test case failed - TC001_AccountRegistrationTest");
+			e.printStackTrace();
+			logger.debug("Debug logs..,");
+			Assert.fail();
+		}
 		
 		
 	}
